8f4c81050ba3e2f61532f94b095b04255bac97a7
czindel <pi@raspberrypi.(none)>
2014-01-09 21:40:01 +0000

    i2c updates

--- app.js
+++ app.js
@@ -5,20 +5,22 @@ var path = require('path');
 var gpio = require('./gpio_export');
 var EventEmitter = require('events').EventEmitter; 
 var i2c = require('i2c');
+var _ = require('underscore');
 
 
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'hjs');
-app.use(express.favicon());
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(require('less-middleware')({ src: __dirname + '/public' }));
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.configure(function(){
+	app.set('port', process.env.PORT || 3000);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'hjs');
+	app.use(express.favicon());
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(require('less-middleware')({ src: __dirname + '/public' }));
+	app.use(express.static(path.join(__dirname, 'public')));  
+})
 
 var server = http.createServer(app)
 var io = require('socket.io').listen(server);
@@ -26,78 +28,118 @@ var io = require('socket.io').listen(server);
 io.set('log level', 1);
 
 server.listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-  initGpio();
-  initI2c();
+	console.log('Express server listening on port ' + app.get('port'));
+	
+	initGpio();
+	initI2c();
 });
 
-var routes = require('./routes/index');
-
-app.get('/', routes.index);
+app.get('/', function(req, res){
+  res.render('index', { title: 'Express' });
+});
 
 var gpioEmitter = new EventEmitter();
 
 io.sockets.on('connection', function (socket) {
-  gpioEmitter.on('buttonPress', function(data){
-    socket.emit('news', data);
-  });
+	gpioEmitter.on('buttonPress', function(data){
+		socket.emit('news', data);
+	});
 });
 
+process.on('SIGINT', function(){
+	gpio.p17_out.writeSync(0)
+	console.log(' -- EXIT -- ');
+
+	process.exit();
+});
 
 
 function initGpio()
 {
-//  var button = gpio.p23_in;
-  var led = gpio.p17_out;
-  led.writeSync(1);
-/*  var count = 0;
+	var button = gpio.p22_in;
+	var led = gpio.p17_out;
+	led.writeSync(1);
 
+	var last, diff = 0.0, current;
+	button.watch(_.debounce(function(err, value) {
+		
+		if( err ) throw err;
 
-  button.watch(function (err, value) {
-    if (err) throw err;
+		if( last )
+		{
+			var diff = process.hrtime(last);
+			console.log(value, diff[0] + diff[1]/1e9);
+		}
+		else{
+			console.log(0);
+		}
 
-    console.log('Button pressed!, its value was ' + value + ' - '+(count++));
+		last = process.hrtime();
+		
+		gpioEmitter.emit('buttonPress', { value: value, time: process.hrtime()});		
 
-    var state = led.readSync() ? 0 : 1;
-
-    gpioEmitter.emit('buttonPress', { state: state });
-
-    led.writeSync(state);
-  });*/
+	}), 50, true);
 }
 
 
 function initI2c()
 {
-  var expander = new i2c(0x38, {device: '/dev/i2c-1'});
-/*  var wire = new i2c();
+	var TSL2561  = new i2c(0x29, {device: '/dev/i2c-1'}); // light sensor
+	var PFC8574  = new i2c(0x38, {device: '/dev/i2c-1'}); // expander
+	var SSD1306  = new i2c(0x3c, {device: '/dev/i2c-1'}); // oled display
+	var ADS1015  = new i2c(0x48, {device: '/dev/i2c-1'}); // A/D converter
+	var MPL115A2 = new i2c(0x60, {device: '/dev/i2c-1'}); // pressure/temperature sensor
+	
+	var wire = new i2c();
+	wire.scan(function(err, data) {
+		
+		data = _.map(data, function(item){
+			return '0x'+item.toString(16);
+		});
 
-  wire.scan(function(err, data) {
-//    console.log('wire.scan 0x', data[0].toString(16));
-    console.log('wire.scan ', data);
-  });*/
+		console.log('wire.scan ', data);
+	});
 
-  //wire.writeByte(0x01, function(err) {});
 
-  var eCount = 0;
-  var offset = 1;
+	/*MPL115A2.readBytes(0x04, 1, function(err, res) {
+		console.log('1of1', res[0])
+	});*/
 
-  var i = setInterval(function(){
+	MPL115A2.readBytes(0x00, 12, function(err, res) {
 
-    var invert = Math.pow(2, eCount) ^ 0xff;
+		//console.log(res.readUInt16BE(0));
+		//console.log(res.readUInt16BE(0));
+		var padc = res.readUInt16BE(0);
+		var tadc = res.readUInt16BE(2);
+		var a0 = res.readUInt16BE(4);
+		var b1 = res.readUInt16BE(6);
+		var b2 = res.readUInt16BE(8);
+		var c12 = res.readUInt16BE(10);
 
-    eCount += offset;
-    expander.writeByte(invert, function(err) {});
+	});
 
-    if( eCount == 7 )
-    {
-      offset = -1;
-    }
 
-    if( eCount == 0 )
-    {
-      offset = 1;
-    }    
-  }, 40);
+	var eCount = 0;
+	var offset = 1;
+
+	setInterval(function(){
+
+		var invert = Math.pow(2, eCount) ^ 0xff;
+
+		eCount += offset;
+		PFC8574.writeByte(invert, function(err) {});
+
+		if( eCount == 7 )
+		{
+			offset = -1;
+		}
+
+		if( eCount == 0 )
+		{
+			offset = 1;
+		}    
+	}, 500);
+
+
 }
 
--- gpio_export.js
+++ gpio_export.js
@@ -1,7 +1,5 @@
 var Gpio = require('onoff').Gpio;
 
-exports.p23_in = new Gpio(23, 'in', 'rising', {
-    persistentWatch : true,
-    debounceTimeout : 100
-  });
-exports.p27_out = new Gpio(27, 'out');
+exports.p22_in  = new Gpio(22, 'in', 'falling');
+
+exports.p17_out = new Gpio(17, 'out');
